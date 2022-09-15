f55db2525636be5e97548d9b0b9e4a16cfe104a7
LStaley <lstaley9185@gmail.com>
2022-09-15 17:03:59 -0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double balance = 5000;
+        double intrestRate = 1.17;
+        double month1 = balance * intrestRate;
+        double month2 = month1 * intrestRate;
+        System.out.println("First months Balance with the intrest rate of " + intrestRate + " is " + month1 );
+        System.out.println("Secound months Balance with the intrest rate of " + intrestRate + " is " + month2 );
+
     }
 }
